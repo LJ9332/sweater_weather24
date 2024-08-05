c08ed24646cfb768056c2b527d3bc813abbbff73
lance butler <lancej2004@gmail.com>
2024-08-05 17:36:18 -0600

    Refactor: refators forecast controller into service, facade, and poro

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,28 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://www.mapquestapi.com") do |faraday|
-      # faraday.headers["MAP_QUEST_API_KEY"] = Rails.application.credentials.api_key[:map_quest]
-    end
-    # add loction as a required query param
-    # response = conn.get("/geocoding/v1/address?location=cincinatti,oh")
-    response = conn.get("/geocoding/v1/address") do |req|
-      req.params['key'] = Rails.application.credentials.api_key[:map_quest]
-      req.params['location'] = "cincinatti,oh"
-    end
-    
-    json = JSON.parse(response.body, symbolize_names: true)[:results]
-    
-    #binding.pry
-    lat = json[:locations][0][:latLng][:lat]
-    lng = json[:locations][0][:latLng][:lng]
-
-    conn = Faraday.new(url: "http://api.weatherapi.com") do |faraday|
-      faraday.headers["WEATHER_API_KEY"] = Rails.application.credentials.api_key[:weather]
-    end
-
-    response = conn.get("/v1/forecast.json") do |req|
-      req.params['q'] = "#{lat},#{lng}"
-      req.params['days'] = 5
-    end
+    forecast = ForecastFacade.new.find_forecast(params[:location])
+    render json: ForecastSerializer.new(forecast)
   end
 end
