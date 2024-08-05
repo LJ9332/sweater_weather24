00801d20a482cf525df4b732f6bb89f99eb273f3
lance butler <lancej2004@gmail.com>
2024-08-05 16:50:55 -0600

    Update: adds fixture files and rails_helper + spec_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,21 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<MAP_QUEST_API_KEY>') { Rails.application.credentials.api_key[:map_quest] }
+  config.filter_sensitive_data('<WEATHER_API_KEY>') { Rails.application.credentials.api_key[:weather] }
+  config.configure_rspec_metadata!
+end
+require 'simplecov'
+SimpleCov.start
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
