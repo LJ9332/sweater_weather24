b4f235edab69f91590cb466b4007c5c351a16da4
lance butler <lancej2004@gmail.com>
2024-08-06 16:00:39 -0600

    Test: creates happy and sad path test for creating users

---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+describe "can create a new user" do
+  it "returns a new user" do
+    user_params = ({
+      email: "test@test.com",
+      password: "test123",
+      password_confirmation: "test123"
+    })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/users", headers: headers, params: JSON.generate(user: user_params)
+    created_user = User.last
+
+    expect(response).to be_successful
+    expect(created_user.email).to eq(user_params[:email])
+    expect(created_user.password).to eq(user_params[:password])
+    expect(created_user.password_digest).to eq(user_params[:password_digest])
+  end
+
+  it "returns a 422 status and error message when missing any required attribute" do  
+    user_params = ({
+      email: "",
+      password: "test123",
+      password_confirmation: "test123"
+    })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/users", headers: headers, params: JSON.generate(user: user_params)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(422)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_an(Array)
+    expect(data[:errors].first[:status]).to eq("422")
+    expect(data[:errors].first[:title]).to eq("Validation failed: Email can't be blank")
+  end
+
+  it "returns a 400 status and error message if password don't match" do
+    user_params = ({
+      email: "",
+      password: "test123",
+      password_confirmation: "test123"
+    })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/users", headers: headers, params: JSON.generate(user: user_params)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_an(Array)
+    expect(data[:errors].first[:status]).to eq("400")
+    expect(data[:errors].first[:title]).to eq("Password confirmation doesn't match Password")
+  end
+end
